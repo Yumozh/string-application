50dbf77ee78fd86a79e984c13b761bdbd5ff7bff
Yumozh <juliiamozhchil@gmail.com>
2026-04-09 13:32:19 -0500

    feat: print information about me

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -7,5 +7,10 @@ public class VariableApp {
         String middleName = " ";
         boolean hasPet = true;
         String niceWords = "Stay Hydrated!";
+
+        System.out.println("My favourite color is: " + myFavouriteColor);
+        System.out.println("I started YearUp program in " + yearStartedProgram);
+        System.out.println("It is " + hasPet + " that I have a pet.");
+        System.out.println(niceWords);
     }
 }
