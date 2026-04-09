e7dcc3d6ec748bf4f96d3d78ab2a50f28f86fdf9
Yumozh <juliiamozhchil@gmail.com>
2026-04-09 13:20:55 -0500

    feat: Initialize commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String myFavouriteColor = "Green";
+        short yearStartedProgram = 2026;
+        String middleName = " ";
+        boolean hasPet = true;
+        String niceWords = "Stay Hydrated!";
+    }
+}
